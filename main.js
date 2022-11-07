fedb86c07c6296550e4d8ada9da8d048d15bab84
Yakin Dario <dario.mendez@alumnos.udg.mx>
2022-11-07 17:40:15 -0600

    add: Menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu')
+;
 
 menuEmail.addEventListener('click', () => {
     console.log('clicked');
     desktopMenu.classList.toggle('inactive');
 } )
+
+menuHamIcon.addEventListener('click', () => {
+    mobileMenu.classList.toggle('inactive');
+})
 
 console.log("holi");
